f9aba052f4cd900c09dcabbfa640820321694b8f
rokie95 <motiurga@gmail.com>
2019-08-29 13:27:01 -0700

    updated PP1.1 to have rioux style

--- PP1.1-Initals.java
+++ PP1.1-Initals.java
@@ -1,11 +1,11 @@
-public class Initials {
-
-    public static void main(String[] args) {
+public class Initials 
+{
+    public static void main(String[] args) 
+    {
         System.out.println(" MMMM       MMMM     GGGG     UU     UU");
         System.out.println(" MM MM     MM MM   GG         UU     UU");
         System.out.println(" MM  MM   MM  MM   GG   GGG   UU     UU");
         System.out.println(" MM   MM MM   MM   GG    GG   UU     UU");
         System.out.println(" MM    MM     MM    GGGGG      UUUUUUU ");
-
     }
 }
