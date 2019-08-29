52454c13b0edd20f4d750ad8cfeafa82bdec83b3
rokie95 <motiurga@gmail.com>
2019-08-29 13:57:47 -0700

    Create PP1.3-Favorites1.java

---
+++ PP1.3-Favorites1.java
@@ -0,0 +1,12 @@
+public class Favorites1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Name: Moti Urga");
+        System.out.println("Birthday: August 17 2004");
+        System.out.println("Hobbies:  Computer Programming and Video Gaming.");
+        System.out.println("Favorite Book: Harry Potter and the Sorcerer's Stone");
+        System.out.println("Favorite Movie: Spy Kids 2: Camp Fred");
+
+    }
+}
