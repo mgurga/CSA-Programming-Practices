55980939c0bb587b26e8c4cf66820e409c862767
rokie95 <motiurga@gmail.com>
2019-08-29 23:24:46 -0700

    changed file structure

---
+++ 1.4/Quotable.java
@@ -0,0 +1,9 @@
+public class Quotable 
+{
+	public static void main(String[] args) 
+	{	
+		System.out.println("\"I know not with what weapons World War III will be fought, but World War IV will be fought with sticks and stones.\"");
+		System.out.println("-Albert Einstien");
+	}
+
+}
---
+++ 1.5/Favorites2.java
@@ -0,0 +1,12 @@
+public class Favorites2
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Name:\tMoti \"HACKERMAN\" Urga");
+        System.out.println("Birthday:\t 8\\17\\2004");
+        System.out.println("Hobbies:\tComputer Programming and Video Gaming");
+        System.out.println("Favorite Book:\t\t\'Harry Potter and the Sorcerer's Stone\'");
+        System.out.println("Favorite Movie:\t\t\"Spy Kids 2: Camp Fred\"");
+
+    }
+}
