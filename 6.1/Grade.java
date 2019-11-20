d39274e2e1d751394a78f9a1566c49d5f094dd0b
rokie95 <motiurga@gmail.com>
2019-11-20 12:15:38 -0800

    added 6.1 code part 2

---
+++ 6.1/Grade.java
@@ -0,0 +1,21 @@
+public class Grade 
+{
+    private int number;
+    private String letter;
+    
+    public Grade(int number, String letter)
+    {
+        this.number = number;
+        this.letter = letter;
+    }
+
+    public int getNumber()
+    {
+        return this.number;
+    }
+
+    public String getLetter()
+    {
+        return this.letter;
+    }
+}
