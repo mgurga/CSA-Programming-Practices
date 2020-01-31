18f0dcca2d076f2163f1b970ed2570d5d4db6de6
rokie95 <motiurga@gmail.com>
2020-01-31 13:36:05 -0800

    added name

--- cw70/Words2.java
+++ cw70/Words2.java
@@ -2,6 +2,9 @@ public class Words2
 {
    public static void main (String[] args)
    {
+      System.out.println("Moti Urga");
+      System.out.println();
+      
       Dictionary2 webster = new Dictionary2 (1500, 52500);
 
       webster.pageMessage();
